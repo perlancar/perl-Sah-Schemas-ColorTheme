dd3fa135a9eddf9e2e7626199ab54d6e815dd9f9
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-20 07:20:02 +0700

    [doc] Mention about 'color schemes' term and Sah::Schemas::ColorScheme (alias for Sah::Schemas::GraphicsColorNames) & Graphics::ColorNames

--- lib/Sah/Schemas/ColorTheme.pm
+++ lib/Sah/Schemas/ColorTheme.pm
@@ -6,6 +6,15 @@ package Sah::Schemas::ColorTheme;
 1;
 # ABSTRACT: Sah schemas related to ColorTheme
 
+=head1 DESCRIPTION
+
+I use "color themes" to refer to modules in the L<ColorTheme>::* (or
+C<WHATEVER::ColorTheme::*>) namespace, and "color schemes" to refer to modules
+in the L<Graphics::ColorNames>::* namespace.
+
+
 =head1 prepend:SEE ALSO
 
 L<ColorTheme>
+
+L<Sah::Schemas::ColorScheme> and L<Graphics::ColorNames>
